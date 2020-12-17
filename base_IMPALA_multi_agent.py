77b1050d42c08ed8d2ae42e40384ea7002ad7d24
bcahlit <bcahlit@gmail.com>
2020-12-17 10:27:39 +0800

    add impala

---
+++ base_IMPALA_multi_agent.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from ray import tune
+from ray.rllib.agents.impala import ImpalaTrainer
+from ray.tune import grid_search
+from gym import spaces
+import numpy as np
+import hfo_py
+
+from soccer_env.mult_agent_env import MultiAgentSoccer
+
+env_config = {
+        "server_config":{
+            "defense_npcs": 0,
+            "offense_agents":2
+        },
+        " feature_set": hfo_py.LOW_LEVEL_FEATURE_SET ,
+    }
+
+def on_episode_end(info):
+    episode = info["episode"]
+    # print("episode.last_info_for()", episode.last_info_for(0))
+    episode.custom_metrics["goal_rate"] = int(episode.last_info_for(0)['status'] == hfo_py.GOAL)
+
+server_config = env_config["server_config"]
+obs_space_size = 59 + 9*(server_config["defense_npcs"]+server_config["offense_agents"]-1)
+obs_space = spaces.Box(low=-1, high=1,
+                                            shape=((obs_space_size,)), dtype=np.float32)
+act_space = spaces.Discrete(14)
+
+def gen_policy(_):
+    return (None, obs_space, act_space, {})
+
+# Setup PPO with an ensemble of `num_policies` different policies
+policies = {
+    'policy_{}'.format(i): gen_policy(i) for i in range(server_config["offense_agents"])
+}
+policy_ids = list(policies.keys())
+
+stop = {
+       "timesteps_total": 100000,
+       "episode_reward_mean": 0.89
+       }
+results = tune.run(ImpalaTrainer, config={
+    "env": MultiAgentSoccer,
+    "env_config": env_config,
+    'multiagent': {
+        'policies': policies,
+        'policy_mapping_fn':
+            lambda agent_id: policy_ids[agent_id],
+    },
+    "callbacks": {
+        "on_episode_end": on_episode_end,
+    },
+    "lr": 0.0002,
+    "num_gpus" : 1,
+    "num_workers": 5,
+    "framework": 'torch'
+}, stop=stop)  
